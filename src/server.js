0ef4233d51a06b324b37909f0b01bfefddd0640d
Kim Ik-Soo <87025474+lynch456@users.noreply.github.com>
2022-03-03 12:21:33 +0900

    ~cleaning code

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
+const PORT = 4000;
+const app = express(); //express 사용하게해주는 어플리케이션 생성
+const logger = morgan("dev");
+app.use(logger);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
