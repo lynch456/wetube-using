6b8a0ccaa961a283fb5b95d2d835df3b0b6c12ab
Kim Ik-Soo <87025474+lynch456@users.noreply.github.com>
2022-03-03 13:02:24 +0900

    export

---
+++ src/controllers/videoController.js
@@ -0,0 +1,9 @@
+export const trending = (req, res) => {
+  res.send("Home Page Videos");
+};
+export const watch = (req, res) => {
+  res.send("Watch");
+};
+export const edit = (req, res) => {
+  res.send("Edit");
+};
